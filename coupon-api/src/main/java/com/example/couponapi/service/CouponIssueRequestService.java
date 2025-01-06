9e82c5720978a8f19dd4bfad256383c42fabec84
진혜민 <hmjin@hdene.co.kr>
2025-01-06 15:26:01 +0900

    git action yml 파일 수정

--- coupon-api/src/main/java/com/example/couponapi/service/CouponIssueRequestService.java
+++ coupon-api/src/main/java/com/example/couponapi/service/CouponIssueRequestService.java
@@ -1,7 +1,6 @@
 package com.example.couponapi.service;
 
 import com.example.couponapi.controller.dto.CouponIssueRequestDto;
-import com.example.couponcore.component.DistributeLockExecutor;
 import com.example.couponcore.service.AsyncCouponIssueServiceV1;
 import com.example.couponcore.service.AsyncCouponIssueServiceV2;
 import com.example.couponcore.service.CouponIssueService;
